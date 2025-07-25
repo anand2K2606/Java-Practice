7acb9127df0d88ec45496384c804e17dec71a577
Anand <anand@subex.com>
2025-07-25 21:12:29 +0530

    Initial commit

--- src/Practise.java
+++ src/Practise.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Practise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter 1st number : ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter 2nd number : ");
+        int num2 = scanner.nextInt();
+
+        int[] num = {num1, num2};
+
+        System.out.println(Arrays.toString(num));
+
+        for (int ab : num)
+        {
+            System.out.println(ab);
+        }
+
+        System.out.println(Arrays.toString(num));
+    }
 }
--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = scanner.nextLine();
+
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed += s.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
 }
