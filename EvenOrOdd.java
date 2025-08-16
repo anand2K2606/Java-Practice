92d5362aa278a65eff3e06e414388582bb8862aa
Anand <anand@subex.com>
2025-08-16 16:26:15 +0530

    Even or Odd Code

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -11,14 +11,11 @@ public class EvenOrOdd {
 
         if (n%2==0)
         {
-            
             System.out.println("Number is Even");
-
         }
         else
         {
             System.out.println("Number is Odd");
-
         }
         
     }
