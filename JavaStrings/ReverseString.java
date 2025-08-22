b07757ed65de65f90168e7b1cf8e2179a563049c
Anand <anand@subex.com>
2025-08-22 22:34:08 +0530

    Added JavaString Directory

---
+++ JavaStrings/ReverseString.java
@@ -0,0 +1,11 @@
+import java.util.scanner;
+
+public class ReverseString
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextString();
+    }
+}
