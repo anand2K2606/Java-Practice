972b9b59b489320ad967c0ef25a9918f4979e175
Anand <anand@subex.com>
2025-08-22 23:28:39 +0530

    Applied String builder logic

--- JavaStrings/ReverseString.java
+++ JavaStrings/ReverseString.java
@@ -8,5 +8,13 @@ public class ReverseString
 
         System.out.print("Enter the String: ");
         String str = sc.next();
+
+        StringBuilder Str2 = new StringBuilder(str).reverse();
+
+        String str3 = String.valueOf(Str2);
+        System.out.println(str3);
+
+        sc.close();
     }
+
 }
