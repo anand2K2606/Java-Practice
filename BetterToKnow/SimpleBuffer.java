9f678bde3dae45b10fc1691f101e528d894cd5a9
Anand <anand@subex.com>
2025-08-24 20:20:08 +0530

    Simple BufferedReader input program

---
+++ BetterToKnow/SimpleBuffer.java
@@ -0,0 +1,26 @@
+package BetterToKnow;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SimpleBuffer
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+
+        System.out.print("Enter the word: ");
+        String input = bf.readLine();
+
+        System.out.println(input);
+
+        // int num = Integer.parseInt(input);
+        // System.out.println(num);
+
+        in.close();
+        bf.close();
+    }
+}
