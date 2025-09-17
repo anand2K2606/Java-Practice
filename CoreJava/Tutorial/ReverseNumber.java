461c87b9c660f2843535bc82493d24a27668b313
Anand <anand@subex.com>
2025-09-17 22:20:07 +0530

    New Learning

---
+++ CoreJava/Tutorial/ReverseNumber.java
@@ -0,0 +1,21 @@
+package CoreJava.Tutorial;
+
+import java.util.Scanner;
+
+public class ReverseNumber
+{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the Desired number: ");
+        int input = sc.nextInt();
+
+        int count = 0;
+        while (count >= input)
+        {
+
+        }
+
+    }
+}
